9e8a2831be8b902b098043b370d34ac2cd2b1b07
SamehAwad <67038060+SamehAwad@users.noreply.github.com>
2022-03-31 19:07:47 +0200

    Update game structure

--- src/js/modules/random-board.js
+++ src/js/modules/random-board.js
@@ -1,5 +1,5 @@
-const validSudoku = require("./valid-board.js");
-const solverSudoku = require("./solve-board.js");
+const solverSudoku = require("./solve-board.js.js");
+const isValidBox = require("./valid-box.js");
 
 // Create board 9*9 with random 10 element
 const createRandomBoard = () => {
@@ -7,43 +7,46 @@ const createRandomBoard = () => {
   let board = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => ""));
   let maxItems = 10;
   // Initialize variables.
-  let fieldX = 0;
-  let fieldY = 0;
+  let row = 0;
+  let col = 0;
   let lenItems = 0;
   let temp = "";
+  let x = 0;
   while (true) {
     // random number between 0 and 8
-    fieldX = Math.floor(Math.random() * 9);
-    fieldY = Math.floor(Math.random() * 9);
+    row = Math.floor(Math.random() * 9);
+    col = Math.floor(Math.random() * 9);
     // random number between 1 and 9
     value = Math.floor(Math.random() * 9) + 1;
     // Check value is valid.
-    temp = board[fieldX][fieldY];
-    board[fieldX][fieldY] = value + "";
-    if (!validSudoku.isValidSudoku(board)) board[fieldX][fieldY] = temp;
+    temp = board[row][col];
+    board[row][col] = value + "";
+    if (!isValidBox.isValidBox(board, row, col)) board[row][col] = temp;
     // Check number of elements added to board.
     lenItems = board.flat().filter((elm) => elm !== "").length;
     if (lenItems >= maxItems) break;
   }
-  board = solverSudoku.solveSudoku(board);
+  // board = solverSudoku.solveSudoku(board);
   return board;
 };
 
 // Set random element in solved board by value ""
 const clearFields = (solvedBoard, len) => {
-  var newBoard = solvedBoard.map((arr) => {
+  let newBoard = solvedBoard.map((arr) => {
     return [...arr];
   });
   let maxItems = len;
-  let fieldX = 0;
-  let fieldY = 0;
+  let row = 0;
+  let col = 0;
   let lenItems = 0;
   while (true) {
-    fieldX = Math.floor(Math.random() * 9);
-    fieldY = Math.floor(Math.random() * 9);
-    newBoard[fieldX][fieldY] = "";
-    lenItems = newBoard.flat().filter((elm) => elm !== "").length;
-    if (lenItems <= maxItems) break;
+    row = Math.floor(Math.random() * 9);
+    col = Math.floor(Math.random() * 9);
+    if (newBoard[row][col] !== "") {
+      newBoard[row][col] = "";
+      lenItems = newBoard.flat().filter((elm) => elm !== "").length;
+      if (lenItems <= maxItems) break;
+    }
   }
   return [newBoard, solvedBoard];
 };
@@ -58,10 +61,16 @@ const selectDifficulty = (level) => {
   } else {
     lenElement = 35;
   }
-  let [newBoard, solvedBoard] = clearFields(createRandomBoard(), lenElement);
+  let newBoard, solvedBoard;
+  while (true) {
+    try {
+      [newBoard, solvedBoard] = clearFields(solverSudoku.solveSudoku(createRandomBoard()), lenElement);
+      break;
+    } catch {}
+  }
   return [newBoard, solvedBoard];
 };
 
-// console.log(selectDifficulty("easy"));
+// selectDifficulty("easy");
 
 exports.selectDifficulty = selectDifficulty;
---
+++ src/js/modules/valid-box.js
@@ -0,0 +1,49 @@
+const isDuplicatesValues = (array) => {
+  arrayFiltering = array.filter((elm) => elm !== ".");
+  let checkUniques = new Set(arrayFiltering);
+  return arrayFiltering.length !== checkUniques.size;
+};
+
+const rowGood = (board, row) => {
+  let rowCheck = isDuplicatesValues(board[row]);
+  if (rowCheck) return false;
+  return true;
+};
+
+const columnGood = (board, col) => {
+  let columnSelect = [];
+  for (let row = 0; row < 9; row++) {
+    columnSelect[row] = board[row][col];
+  }
+  let columnCheck = isDuplicatesValues(columnSelect);
+  if (columnCheck) return false;
+  return true;
+};
+
+const helperBoxes = (num) => {
+  if (num >= 0 && num <= 2) return 0;
+  if (num >= 3 && num <= 5) return 3;
+  return 6;
+};
+
+const boxesGood = (board, row, col) => {
+  let selectRow = helperBoxes(row);
+  let selectCol = helperBoxes(col);
+
+  let boxesCheck = [];
+  for (let i = selectRow; i < selectRow + 3; i++) {
+    for (let j = selectCol; j < selectCol + 3; j++) {
+      boxesCheck.push(board[i][j]);
+    }
+    if ((i + 1) % 3 === 0) {
+      let tempArray = boxesCheck;
+      tempArray = isDuplicatesValues(tempArray);
+      if (tempArray) return false;
+    }
+  }
+  return true;
+};
+
+const isValidBox = (board, row, col) => {
+  return rowGood(board, row) && columnGood(board, col) && boxesGood(board, row, col);
+};
